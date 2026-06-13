69b6970725087630a6f1392f698e6bbec640dc78
David Mur2304 <davidmurtapia@gmail.com>
2026-06-13 11:58:44 +0200

    db

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,19 @@
 <?php
 // Conexión a la base de datos
-$host = "localhost";
-$user = "u205971234_medico_user";
-$pass = "";
-$dbname = "u205971234_medico_db";
+
+$envPhpPath = __DIR__ . '/../.env.php';
+$envPath = __DIR__ . '/../.env';
+
+if (file_exists($envPhpPath)) {
+    require_once $envPhpPath;
+} elseif (file_exists($envPath)) {
+    require_once $envPath;
+}
+
+$host = defined('DB_HOST') ? DB_HOST : 'localhost';
+$user = defined('DB_USER') ? DB_USER : 'root';
+$pass = defined('DB_PASS') ? DB_PASS : 'root';
+$dbname = defined('DB_NAME') ? DB_NAME : 'medicodb';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
