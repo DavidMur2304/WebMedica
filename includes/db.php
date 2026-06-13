8faa52a2311db7870f999e568eba89eb6ef55440
David Mur2304 <davidmurtapia@gmail.com>
2026-06-13 11:56:50 +0200

    db

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,16 @@
 <?php
-// Configuración de la conexión a la base de datos
+// Conexión a la base de datos
 $host = "localhost";
 $user = "u205971234_medico_user";
-$pass = "dOZV!hB|S7";
+$pass = "";
 $dbname = "u205971234_medico_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Verificar si hay error de conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-// Establecer el charset de la conexión
 $conn->set_charset("utf8");
 
 ?>
